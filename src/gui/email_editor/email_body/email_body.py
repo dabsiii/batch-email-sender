31db3612e9a1a5efb345541a29fe6427f52d5949
Elyson <elysonpanolino@gmail.com>
2024-11-28 20:37:53 +0800

    feat(gui): Implement `EmailBody` interface and initial `EmailBodyC1` class

---
+++ src/gui/email_editor/email_body/email_body.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class EmailBody(ABC):
+    @abstractmethod
+    def apply_bold(self): ...
+
+    @abstractmethod
+    def apply_italic(self): ...
+
+    @abstractmethod
+    def apply_underline(self): ...
+
+    @abstractmethod
+    def change_font_size(self): ...
+
+    @abstractmethod
+    def highlight_variables(self, variables: List[str]): ...
+
+    @abstractmethod
+    def get_html(self): ...
