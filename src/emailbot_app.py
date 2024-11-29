e9544c61fbb77459d26e33ad20fbe916e4f3797e
Elyson <elysonpanolino@gmail.com>
2024-11-29 17:05:36 +0800

    feat(app): Implement email sending with dynamic placeholders and attachments
    - Added `_attachment_folder_path` to store the selected attachment folder path.
    - Enhanced `_on_send_email_click` to handle email sending logic:
      - Connect to the email bot.
      - Process data records and send emails dynamically.
      - Attach files from the selected folder path.
    - Introduced `_replace_variables` method for substituting placeholders with record values.
    - Ensured proper disconnection from the email bot in the `finally` block.
    
    This update enables dynamic email composition and attachment handling, improving the app's functionality for bulk email operations.

--- src/emailbot_app.py
+++ src/emailbot_app.py
@@ -1,4 +1,5 @@
-from typing import List
+from pathlib import Path
+from typing import Dict, List
 
 from src.gui.email_bot_gui_c1 import EmailBotGuiC1
 from src.model.credentials import Credentials
@@ -12,6 +13,7 @@ class EmailBotApp:
         self._variables: List = None
         self._credentials: Credentials = None
         self._data: Data = None
+        self._attachment_folder_path: Path = None
         self._email_bot: EmailBot = None
         self._gui = EmailBotGuiC1()
         self._gui.selected_credentials.subscribe(self._read_credentials)
@@ -39,9 +41,47 @@ class EmailBotApp:
         self._gui.update_variables(self._variables)
 
     def _read_attachment_folder(self, data) -> None:
-        path = self._gui.get_attachment_folder_path()
-        print(path)
+        self._attachment_folder_path = self._gui.get_attachment_folder_path()
 
     def _on_send_email_click(self, data) -> None:
-        body = self._gui.get_email_body_html()
-        print(body)
+        credentials = self._credentials
+        data = self._data
+        attachment_folder_path = self._attachment_folder_path
+
+        username = credentials.get_username()
+        password = credentials.get_password()
+        email_bot = EmailBot(username, password)
+
+        try:
+            email_bot.connect()
+
+            for record in data.get_records():
+                email = record["EMAIL"]
+                file = record["FILE"]
+
+                attachment = f"{attachment_folder_path}\\{file}"
+                raw_html_body = self._gui.get_email_body_html()
+                raw_subject = self._gui.get_email_subject()
+                email_bot.send_email(
+                    recipient=email,
+                    subject=self._replace_variables(
+                        raw_subject, self._variables, record
+                    ),
+                    body=self._replace_variables(
+                        raw_html_body, self._variables, record
+                    ),
+                    is_html=True,
+                    attachments=[attachment],  # Update paths
+                )
+        finally:
+            email_bot.disconnect()
+
+    def _replace_variables(self, text: str, variables: List[str], record: Dict) -> str:
+        out_text = text
+        for variable in variables:
+            substring = f"{{{variable}}}"
+            if out_text.find(substring) != -1:
+                value = record[variable]
+                out_text: str = out_text.replace(substring, str(value))
+
+        return out_text
