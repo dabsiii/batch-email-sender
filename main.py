dc7ea689be767afcc4d82699f5786daba1096a62
Elyson <elysonpanolino@gmail.com>
2024-11-29 08:47:52 +0800

    feat(gui): Enhance `EmailBotGuiC1` with selectors and Gmail editor integration
    - Updated `main.py` to use `gui.show()` instead of `app.start()` for launching the GUI.
    - Enhanced `EmailBotGuiC1` by:
      - Adding a horizontal layout to organize GUI components.
      - Introducing a selectors section (`QFrame`) for user inputs:
        - Credential selector for importing JSON files.
        - Data selector for choosing Excel files.
        - Folder selector for specifying an attachment directory.
      - Integrating `EmailEditorC1` as the Gmail editor component.
    - Improved modularity and clarified component roles with layout restructuring.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src.gui.email_bot_gui_c1 import EmailBotGuiC1
 
-app = EmailBotGuiC1()
-app.start()
+gui = EmailBotGuiC1()
+gui.show()
--- src/gui/email_bot_gui_c1.py
+++ src/gui/email_bot_gui_c1.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout, QVBoxLayout, QWidget
 
 from src.gui.email_bot_gui import EmailBotGui
 from src.gui.email_editor.email_editor_c1 import EmailEditorC1
@@ -13,6 +13,9 @@ from src.gui.selector.folder_selector import FolderSelector
 class EmailBotGuiC1(EmailBotGui):
     """
     A. Selectors
+        A.1 Credentials
+        A.2 Data
+        A.3 Attachment folder
     B. Gmail Editor
     C. Send Email
     """
@@ -22,8 +25,41 @@ class EmailBotGuiC1(EmailBotGui):
 
     def _init_gui(self) -> None:
         self._widget = QWidget()
-        self._layout = QVBoxLayout()
-        self._widget.setLayout(self._layout)
+        self._widget_layout = QHBoxLayout()
+        self._widget.setLayout(self._widget_layout)
+
+        # A Selectors
+        self._selectors_frame = QFrame()
+        self._selectors_frame_layout = QVBoxLayout()
+        self._selectors_frame.setLayout(self._selectors_frame_layout)
+        self._widget_layout.addWidget(self._selectors_frame)
+
+        # A.1 Credentials
+        self._credential_selector = FileSelector(
+            info_text="Import Credentials (.json file)",
+            icon_path=Path("assets\\json-icon.png").resolve(),
+            filter="JSON Files (*.json);;All Files (*)",
+        )
+        self._selectors_frame_layout.addWidget(self._credential_selector.widget)
+
+        # A.2 Data
+        self._data_selector = FileSelector(
+            info_text="Select a data file (.xlsx file)",
+            icon_path=Path("assets\\xlsx-icon.png").resolve(),
+            filter="Excel Files (*.xls *.xlsx);;All Files (*)",
+        )
+        self._selectors_frame_layout.addWidget(self._data_selector.widget)
+
+        # 3 Attachment Folder
+        self._attachment_folder_selector = FolderSelector(
+            info_text="Select a Folder containing the attachment",
+            icon_path=Path("assets\\folder-icon.jpg").resolve(),
+        )
+        self._selectors_frame_layout.addWidget(self._attachment_folder_selector.widget)
+
+        # B. Gmail Editor
+        self._email_editor = EmailEditorC1()
+        self._widget_layout.addWidget(self._email_editor.widget)
 
     def show(self) -> None:
         app = QApplication(sys.argv)
